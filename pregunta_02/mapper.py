66751a3c64c7592fd86c13258d174d81bbd5a111
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-06-14 23:36:30 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        word = line.split(',')
+        sys.stdout.write("{}\t{}\n".format(word[3],word[4]))
