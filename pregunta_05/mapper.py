c4b32ab3cdd93bfb14f36d94f2708fbffd6095df
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-06-15 22:43:01 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
 
     for line in sys.stdin:
         data = (line.split("   ")[1].split('-')[1])
-        sys.stdout.write("{},1\n".format(data))
+        sys.stdout.write("{}\t1\n".format(data))
