b3a2bdffbd1de90ef6eeeb2f113644990fdc679a
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-06-15 23:14:45 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    max = 0
+    min = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split('\t')
+        val = float(val)
+
+        if key == curkey:
+            if val >= max:
+                max = val
+            if val <= min:
+                min = val
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, max, min))
+
+            curkey = key
+            max = val
+            min = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, max, min))
