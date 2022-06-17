2c5a3ae19a98307c320f9006d0d8faa1e58f8801
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-06-16 22:43:19 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,34 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+
+    sum = 0
+    avg = 0
+    cont = 0
+
+    for line in sys.stdin:
+
+        key, val, val2 = line.split('\t')
+        val = float(val)
+        val2 = float(val2)
+
+        if key == curkey:
+
+            sum += val
+            cont += val2
+            avg = sum/cont
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum, avg))
+
+            curkey = key
+            sum = val
+            cont = val2
+            avg = sum/cont
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum, avg))
