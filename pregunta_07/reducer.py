7103de670f6d46c160be6b5f1fae54eb42a233d6
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-06-16 22:11:33 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    elements = []
+
+    for line in sys.stdin:
+        elements.append(line)
+else:
+    elements = sorted(
+                elements,
+                key = lambda data: (data,split()[0], int(data.split()[2]))
+                )
+
+    for element in elements:
+        sys.stdout.write("{}".format(element))
