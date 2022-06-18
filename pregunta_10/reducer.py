e3d9816f8111c85f3bbc79d4d57c176633937c1f
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-06-18 16:40:19 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = 0
+
+    for line in sys.stdin:
+        key, val = line.split()
+        val = val.strip()
+
+        if key == curkey:
+            cadena = cadena + ',' + val
+            lista = cadena.split(',')
+            lista = sorted(int(i) for i in lista)
+            lista = ','.join(str(i) for i in lista)
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\n".format(curkey, lista))
+
+            curkey = key
+            cadena = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, lista))
